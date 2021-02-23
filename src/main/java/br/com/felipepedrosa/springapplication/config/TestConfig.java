a43b9a3cc0d30e4d18deeddd0245f49e833c2b1c
Felipe Pedrosa <felipeepedrosa@gmail.com>
2021-02-22 22:28:22 -0300

    Created entity Product

--- src/main/java/br/com/felipepedrosa/springapplication/config/TestConfig.java
+++ src/main/java/br/com/felipepedrosa/springapplication/config/TestConfig.java
@@ -2,10 +2,12 @@ package br.com.felipepedrosa.springapplication.config;
 
 import br.com.felipepedrosa.springapplication.entities.Category;
 import br.com.felipepedrosa.springapplication.entities.Order;
+import br.com.felipepedrosa.springapplication.entities.Product;
 import br.com.felipepedrosa.springapplication.entities.User;
 import br.com.felipepedrosa.springapplication.entities.enums.OrderStatus;
 import br.com.felipepedrosa.springapplication.repositories.CategoryRepository;
 import br.com.felipepedrosa.springapplication.repositories.OrderRepository;
+import br.com.felipepedrosa.springapplication.repositories.ProductRepository;
 import br.com.felipepedrosa.springapplication.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     /**
      * {@inheritDoc}
      */
@@ -52,5 +57,13 @@ public class TestConfig implements CommandLineRunner {
         Category c2 = new Category(null, "Books");
         Category c3 = new Category(null, "Computers");
         categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
     }
 }
